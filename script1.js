2449b1b6002b2a16bc7207c1ae167305d7a2a84d
mgompia <m.gomez.piatchek@iescristobaldemonroy.es>
2026-09-22 10:41:45 +0200

    primeros tres ejemplos

--- script1.js
+++ script1.js
@@ -1,1 +1,18 @@
-console.log("Hola Mundo");
+//Ejemplo 1
+console.log("Hola mundo");
+
+//Ejemplo 2
+var nombre = "Martin";
+console.log("Hola " + nombre);
+
+//Ejemplo 3
+var nombre = window.prompt("Introduce tu nombre: ");
+console.log("Hola " + nombre);
+
+//Ejemplo 4
+var edad = window.prompt("Introduce tu edad: ");
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("No eres mayor de edad");
+}   
